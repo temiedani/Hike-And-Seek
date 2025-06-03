85ec631f44c94a95425943f3ef25c4ec7eba4a7d
Temesgen Teklebrhan <temesgen.teklebrhan@jaid.io>
2025-06-04 00:34:18 +0100

    Update base path to match GitHub Pages URL format

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Hike%20And%20Seek/",
+  base: "/Hike-And-Seek/",
 });
